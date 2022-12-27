f914696cbe42f0ef8ced7e1925648378686a1034
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2022-12-27 11:02:37 +0200

    updated proxy endpoint

--- next-gen-residential-proxies/CSharp/Oxylabs/Settings.cs
+++ next-gen-residential-proxies/CSharp/Oxylabs/Settings.cs
@@ -14,6 +14,6 @@ namespace Oxylabs
         public readonly static String URL_LIST_NAME = "./files/url_list_residential.txt";
         public readonly static String BROWSER_LIST_NAME = "./files/browser_headers.json";
         public readonly static String AGENT_LIST_NAME = "./files/user_agents.json";
-        public readonly static String PROXY_ADDRESS = "ngrp.oxylabs.io:60000";
+        public readonly static String PROXY_ADDRESS = "unblock.oxylabs.io:60000";
     }
 }
