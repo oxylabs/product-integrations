16a2504be64e8e136cef7dda87c9292d5832f90f
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2023-12-13 16:14:54 +0200

    Update shell.sh

--- web-unblocker/Shell/shell.sh
+++ web-unblocker/Shell/shell.sh
@@ -1,1 +1,1 @@
-shell curl -k --proxy unblock.oxylabs.io:60000 --proxy-user username:pass "https://ip.oxylabs.io"
+shell curl -k --proxy unblock.oxylabs.io:60000 --proxy-user username:pass "https://ip.oxylabs.io/location"
